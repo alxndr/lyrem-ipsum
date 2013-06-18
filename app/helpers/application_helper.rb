c885de1d8d5baaad425f781aad9c4891dc2e7377
Alexander <alxndr+github@gmail.com>
2013-06-18 07:43:29 -0700

    homegrown helper for now
    [finishes #15]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def google_analytics_code
+    %Q{<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', '#{LyremIpsum::Application.config.google_analytics_tracking_id}', '#{request.host}>');
+      ga('send', 'pageview');
+    </script>} if LyremIpsum::Application.config.google_analytics_tracking_id
+  end
 end
