eaf8610e13d31e99e8b3a0014d684e1981d11fa6
Alexander <alxndr+github@gmail.com>
2013-06-19 12:03:01 -0700

    start versioning: 0.0.1
    http://code.dooleydoes.com/2013/03/rails-app-versioning-with-git-tags.html

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,15 @@ Bundler.require(:default, Rails.env)
 
 module LyremIpsum
   class Application < Rails::Application
+
+    if Rails.env.development?
+      # Update version file from latest git tag
+      File.open('config/version', 'w') do |file|
+        file.write `git describe --tags --always`
+      end
+    end
+    config.version = File.read('config/version')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
