316e0c790e271b8178f46615df6f4aee5665d8bd
Alexander <alxndr+github@gmail.com>
2013-09-04 22:00:40 -0700

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+  protect_from_forgery with: :exception # Prevent CSRF attacks by raising an exception. For APIs, you may want to use :null_session instead.
+
+  before_filter :check_uri_host
+
+  def check_uri_host
+    # TODO request test
+    if Rails.env == 'production' && !params[:no_redirect] && request.host != 'lyrem-ipsum.com'
+      redirect_to request.protocol + request.host_with_port + request.request_uri
+    end
+  end
+
 end
+
