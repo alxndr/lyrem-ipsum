eba33e039dbfbff8921db5f8f2fbf58b688f36fa
Alexander <alxndr+github@gmail.com>
2013-09-04 22:32:13 -0700

    skylight in staging too

--- config/application.rb
+++ config/application.rb
@@ -22,21 +22,12 @@ module LyremIpsum
     #end
     #config.version = File.read('config/version')
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
     # "Due to a change in Rails that prevents images from being compiled in vendor and lib, you'll
     # need to add the following line to your application.rb:"
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    # hook in to Skylight.io on staging as well as prod
+    config.skylight.environments << 'staging'
 
   end
 end
