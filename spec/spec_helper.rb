a5c2042233184adedeb9d3c951c1ea9c26d02318
Alexander <alxndr+github@gmail.com>
2013-10-27 16:18:58 -0700

    add some unit testing of LyricsWiki
    also removed HTTParty due to difficulty with mocking
    [fixes #4]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ require 'rspec/autorun'
 require 'webmock/rspec'
 require 'capybara/rspec'
 
+WebMock.disable_net_connect!(allow_localhost: true)
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
