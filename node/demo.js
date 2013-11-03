c86b32ea947ddc9d3435b794bbdbdc5b9a779ba1
Alexander <alxndr+github@gmail.com>
2013-11-02 17:20:02 -0700

    tweaking, package.json

--- node/demo.js
+++ node/demo.js
@@ -4,7 +4,7 @@ Array.prototype.random = function() {
 Array.prototype.times_do = function(callback) {
   return Array.apply(null, Array(this[0])).map(callback); // todo - add i param to callback
 };
-var lyrics_wiki = require('./lyrics_fetcher_promise');
+var lyrics_wiki = require('./lyrics_wiki_proxy');
 
 // todo - proper objecty things
 function random_song(artist) {
@@ -19,7 +19,7 @@ function artist_name(artist) {
 
 var pick_random_song = function(artist) {
   console.log(artist_name(artist));
-  return lyrics_wiki.fetch_song_data(artist_name(artist), random_song(artist_name(artist)));
+  return lyrics_wiki.fetch_song_data(artist_name(artist), random_song(artist));
 };
 var pick_n_random_songs = function(artist, n) {
   return [n].times_do(function() { return random_song(artist); });
@@ -34,20 +34,17 @@ var get_lyrics = function(song) {
 };
 
 lyrics_wiki.fetch_artist_data('phish')
-  .then(function(artist) { return pick_n_random_songs(artist, 10); })
+  .then(function(artist) { console.log(artist_name(artist)); return pick_n_random_songs(artist, 10); })
   .then(function(songs) {
+    console.log('here are ten random songs');
     console.log(songs);
+    console.log();
   }, function(err) {
     console.log('error!', err);
   });
 
-/*
 lyrics_wiki.fetch_artist_data('phish')
   .then(pick_random_song)
   .then(get_lyrics)
-  .then(function(lyrics) {
-    console.log(lyrics);
-  }, function(err) {
-    console.log('error!', err);
-  });
-*/
+  .then(function(retval) { console.log(retval); }, function(err) { console.log('error!', err); })
+;
