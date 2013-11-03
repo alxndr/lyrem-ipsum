e7e9a52637d4d56f03e8c5158b8476ecdfaed5d0
Alexander <alxndr+github@gmail.com>
2013-11-02 17:52:10 -0700

    moving things around

--- node/demo.js
+++ node/demo.js
@@ -6,35 +6,8 @@ Array.prototype.times_do = function(callback) {
 };
 var lyrics_wiki = require('./lyrics_wiki_proxy');
 
-// todo - proper objecty things
-function random_song(artist) {
-  return artist.albums.random().songs.random();
-}
-function song_name(song) {
-  return song.song;
-}
-function artist_name(artist) {
-  return artist.artist;
-}
-
-var pick_random_song = function(artist) {
-  console.log(artist_name(artist));
-  return lyrics_wiki.fetch_song_data(artist_name(artist), random_song(artist));
-};
-var pick_n_random_songs = function(artist, n) {
-  return [n].times_do(function() { return random_song(artist); });
-};
-var get_lyrics = function(song) {
-  console.log('"%s"', song_name(song));
-  console.log();
-  if (song.lyrics.match(/^(Not found|Instrumental)$/)) {
-    throw '...no lyrics found';
-  }
-  return lyrics_wiki.fetch_lyrics(song.url);
-};
-
 lyrics_wiki.fetch_artist_data('phish')
-  .then(function(artist) { console.log(artist_name(artist)); return pick_n_random_songs(artist, 10); })
+  .then(function(artist) { console.log(lyrics_wiki.artist_name(artist)); return lyrics_wiki.pick_n_random_songs(artist, 10); })
   .then(function(songs) {
     console.log('here are ten random songs');
     console.log(songs);
@@ -44,7 +17,19 @@ lyrics_wiki.fetch_artist_data('phish')
   });
 
 lyrics_wiki.fetch_artist_data('phish')
-  .then(pick_random_song)
-  .then(get_lyrics)
+  .then(lyrics_wiki.pick_random_song)
+  .then(lyrics_wiki.get_lyrics)
   .then(function(retval) { console.log(retval); }, function(err) { console.log('error!', err); })
 ;
+
+/*
+lyrics_wiki.fetch_artist_data('phish')
+  .then(function(artist) { console.log(lyrics_wiki.artist_name(artist)); return lyrics_wiki.pick_n_random_songs(artist, 10); })
+  .then(lyrics_wiki.get_lyrics_of_songs)
+  .then(function(retval) {
+    console.log();
+    console.log('done!');
+    console.log();
+    console.log(retval);
+  });
+*/
