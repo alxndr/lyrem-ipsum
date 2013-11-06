de2b234b669b53a83833366db01f95bc6b2414fd
Alexander <alxndr+github@gmail.com>
2013-11-05 22:24:51 -0800

    fixing lyrics_wiki_proxy.get_lyrics_of_songs
    some name changes to avoid confusion
    moving base class prototype things into toolkit.js

--- node/demo.js
+++ node/demo.js
@@ -1,30 +1,14 @@
-Array.prototype.random = function() {
-  return this[ Math.floor(Math.random() * this.length) ];
-};
-Array.prototype.times_do = function(callback) {
-  return Array.apply(null, Array(this[0])).map(callback); // todo - add i param to callback
-};
+require('./toolkit');
 var lyrics_wiki = require('./lyrics_wiki_proxy');
-
+/*
 lyrics_wiki.fetch_artist_data('phish')
-  .then(function(artist) { console.log(lyrics_wiki.artist_name(artist)); return lyrics_wiki.pick_n_random_songs(artist, 10); })
-  .then(function(songs) {
-    console.log('here are ten random songs');
-    console.log(songs);
-    console.log();
-  }, function(err) {
-    console.log('error!', err);
-  });
-
-lyrics_wiki.fetch_artist_data('phish')
-  .then(lyrics_wiki.pick_random_song)
+  .then(lyrics_wiki.pick_random_song_name)
   .then(lyrics_wiki.get_lyrics)
   .then(function(retval) { console.log(retval); }, function(err) { console.log('error!', err); })
 ;
-
-/*
+*/
 lyrics_wiki.fetch_artist_data('phish')
-  .then(function(artist) { console.log(lyrics_wiki.artist_name(artist)); return lyrics_wiki.pick_n_random_songs(artist, 10); })
+  .then(function(artist) { console.log(lyrics_wiki.artist_name(artist)); return lyrics_wiki.pick_n_random_song_names(artist, 10); })
   .then(lyrics_wiki.get_lyrics_of_songs)
   .then(function(retval) {
     console.log();
@@ -32,4 +16,3 @@ lyrics_wiki.fetch_artist_data('phish')
     console.log();
     console.log(retval);
   });
-*/
--- node/lyrics_wiki_proxy.js
+++ node/lyrics_wiki_proxy.js
@@ -1,11 +1,4 @@
-Array.prototype.unique = function() {
-  return this.reduce(function(prev_val, current_val) {
-    if (prev_val.indexOf(current_val) < 0) {
-      prev_val.push(current_val);
-    }
-    return prev_val;
-  }, []);
-};
+require('./toolkit');
 var Q = require('q');
 var Qrequest = Q.denodeify(require('request'));
 var cheerio = require('cheerio');
@@ -13,25 +6,33 @@ var cheerio = require('cheerio');
 function random_song_name_from_artist(artist) {
   return artist.albums.random().songs.random();
 }
-function song_name(song) {
+function get_song_name(song) {
   return song.song;
 }
 function artist_name(artist) {
   return artist.artist;
 }
-var pick_random_song = function(artist) {
+var pick_random_song_name = function(artist) {
   console.log(artist_name(artist));
   return fetch_song_data(artist_name(artist), random_song_name_from_artist(artist));
 };
-var pick_n_random_songs = function(artist, n) {
-  return [n].times_do(function() { return random_song_name_from_artist(artist); });
+var pick_n_random_song_names = function(artist, n) {
+  return [n].times_do(function() {
+    return random_song_name_from_artist(artist);
+  });
 };
 
-var parse_json_in_body = function(response) { return JSON.parse(response[0].body); };
+var parse_json_in_body = function(response) {
+  return JSON.parse(response[0].body);
+};
 var request_url_then_resolve_deferred = function(config) {
   Qrequest(config.url).then(
-    function() { config.deferred.resolve(config.callback.apply(null, arguments)); },
-    function() { config.deferred.reject(config.error, arguments); }
+    function() {
+      config.deferred.resolve(config.callback.apply(null, arguments));
+    },
+    function() {
+      config.deferred.reject(config.error, arguments);
+    }
   );
 };
 
@@ -72,12 +73,11 @@ var fetch_song_data = function(artist_name, song_name) {
 };
 
 var fetch_lyrics = function(lyrics_url) {
-  console.log('fetch_lyrics');
   // returns promise
   var deferred = Q.defer();
 
   if (!lyrics_url) {
-    deferred.reject('missing song lyrics url',lyrics_url);
+    deferred.reject('missing song lyrics url', lyrics_url);
     return deferred.promise;
   }
 
@@ -92,46 +92,37 @@ var fetch_lyrics = function(lyrics_url) {
 };
 
 var get_lyrics = function(song) {
-  console.log('get_lyrics',song);
-  console.log('"%s"', song_name(song));
+  console.log('get_lyrics', song);
+  console.log('"%s"', get_song_name(song));
   console.log();
-  if (!song.lyrics) {
-    throw 'no lyrics found for: ' + song
-  }
-  if (song.lyrics.match(/^(Not found|Instrumental)$/)) {
-    throw '...no lyrics found';
+  if (!song.lyrics || song.lyrics.match(/^(Not found|Instrumental)$/)) {
+    return [''];
   }
   return fetch_lyrics(song.url);
 };
 
 var get_lyrics_of_songs = function(songs) {
-  var promises = [];
-  console.log('songs', songs);
+  console.log('getting lyrics of songs:', songs);
 
-  songs.forEach(function(song) {
-    console.log('iterating song', song);
+  return Q.all(songs.map(function(song_name) {
     var deferred = Q.defer();
-    console.log('deferred',deferred);
-
-    get_lyrics(song)
-      .then(fetch_lyrics(song.url))
+    fetch_song_data('phish', song_name) // todo - how to get artist there
+      .then(get_lyrics)
       .then(function(lyrics) {
-        console.log('resolving get_lyrics');
         deferred.resolve(lyrics)
       }, function(error) {
-        console.log('rejecting get_lyrics');
         deferred.reject('error', error);
       });
-
-    promises.push(deferred.promise);
-  });
-
-  return Q.all(promises);
-// return Q.all(promises.map(...));
+    return deferred.promise;
+  }));
 };
 
-var is_text_node = function(_i,$node) { return $node.type == 'text'; };
-var get_text = function() { return this.text(); };
+var is_text_node = function(_i, $node) {
+  return $node.type == 'text';
+};
+var get_text = function() {
+  return this.text();
+};
 var extract_lyrics = function(response) {
   var $ = cheerio.load(response[0].body);
   var $contents = $('div.lyricbox').contents();
@@ -140,8 +131,12 @@ var extract_lyrics = function(response) {
   return sanitize_lyrics(dirty_lyrics);
 };
 
-var trim = function(str) { return str.trim(); };
-var is_present = function(str) { return str.length > 0; };
+var trim = function(str) {
+  return str.trim();
+};
+var is_present = function(str) {
+  return str.length > 0;
+};
 var sanitize_lyrics = function(dirty_lyrics) {
   return dirty_lyrics.map(trim).unique().filter(is_present);
 };
@@ -154,6 +149,6 @@ if (exports) {
   exports.get_lyrics = get_lyrics;
   exports.get_lyrics_of_songs = get_lyrics_of_songs;
   exports.artist_name = artist_name;
-  exports.pick_n_random_songs = pick_n_random_songs;
-  exports.pick_random_song = pick_random_song;
+  exports.pick_n_random_song_names = pick_n_random_song_names;
+  exports.pick_random_song_name = pick_random_song_name;
 }
