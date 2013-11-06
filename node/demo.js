e70a8d4358d7b6cdd1433992ff973c0a1cc6d0e9
Alexander <alxndr+github@gmail.com>
2013-11-05 23:56:44 -0800

    node.js demo: counts lines in ten random songs

--- node/demo.js
+++ node/demo.js
@@ -8,11 +8,23 @@ lyrics_wiki.fetch_artist_data('phish')
 ;
 */
 lyrics_wiki.fetch_artist_data('phish')
-  .then(function(artist) { console.log(lyrics_wiki.artist_name(artist)); return lyrics_wiki.pick_n_random_song_names(artist, 10); })
+  .then(log('artist', function(v) {return lyrics_wiki.artist_name(v);}))
+  .then(function(artist) {
+    return lyrics_wiki.pick_n_random_song_names(artist, 10); })
+  .then(log('song names'))
   .then(lyrics_wiki.get_lyrics_of_songs)
-  .then(function(retval) {
-    console.log();
-    console.log('done!');
-    console.log();
-    console.log(retval);
-  });
+  .then(function(lyrics) {
+    console.log('in those ' + lyrics.length + ' random songs');
+    var total_lines = lyrics.map(function(r) { return(r.length); }).reduce(function(prev, cur){ return prev + cur;},0);
+    console.log('there are ' + total_lines + ' unique* lines');
+    return lyrics;
+  })
+  .done()
+;
+
+function log(msg, cb) {
+  return function(val) {
+    console.log(msg,(typeof cb == 'function') ? cb(val) : val);
+    return val;
+  };
+}
