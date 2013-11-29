36aa87d0f2e5e5d878c57aea1148bd7252c15522
Alexander <alxndr+github@gmail.com>
2013-11-29 15:29:09 -0800

    backfilling routing test
    [closes #6]

--- spec/routing/static_routing_spec.rb
+++ spec/routing/static_routing_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe StaticController do
   describe 'routing' do
 
-    describe 'index' do
+    describe '/' do
       it 'should route to StaticController#index' do
         expect(get: '/').to route_to(controller: 'static', action: 'index')
       end
     end
 
-    describe 'health' do
-      it 'should route to health check' do
+    describe '/health' do
+      it 'should route to StaticController#health' do
         expect(get: '/health').to route_to(controller: 'static', action: 'health')
       end
     end
