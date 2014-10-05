384066e022b4fa5cdaabef6eb2061bc7ecaf3ccf
Alexander <alxndr+github@gmail.com>
2014-10-04 23:54:12 -0700

    spinner
    [#38]

--- app/assets/javascripts/views/form_view.js
+++ app/assets/javascripts/views/form_view.js
@@ -1,6 +1,10 @@
 /* global define */
 
-define(['backbone', 'views/artist_suggestions_view'], function(Backbone, ArtistSuggestionsView) {
+define([
+  'backbone',
+  'views/artist_suggestions_view',
+  'spinner'
+], function(Backbone, ArtistSuggestionsView, Spinner) {
 
   return Backbone.View.extend({
 
@@ -13,10 +17,13 @@ define(['backbone', 'views/artist_suggestions_view'], function(Backbone, ArtistS
     render: function() {
       this.artist_input_view = new ArtistSuggestionsView({el: this.el});
       this.artist_input_view.render();
+      this.$button = this.$('button');
       return this;
     },
 
     formSubmitted: function() {
+      this.$button.prop('disabled', 'disabled');
+      new Spinner().spin(this.$el[0]);
       this.artist_input_view.ensure_val();
     }
 
