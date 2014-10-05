3fc4a02e7e456dfa545fa75852aa2baaf5d94a4e
Alexander <alxndr+github@gmail.com>
2014-10-04 21:58:25 -0700

    remove console.log()s

--- app/assets/javascripts/views/form_view.js
+++ app/assets/javascripts/views/form_view.js
@@ -1,14 +1,10 @@
-/* global console, define */
+/* global define */
 
 define(['backbone', 'views/artist_suggestions_view'], function(Backbone, ArtistSuggestionsView) {
 
   return Backbone.View.extend({
 
     el: 'form',
-
-    initialize: function() {
-      console.log("FV initialize");
-    },
 
     render: function() {
       this.artist_input_view = new ArtistSuggestionsView({el: this.el});
