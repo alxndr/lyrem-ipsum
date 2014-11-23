336b66499f47ae4757536fd16490d43ab8c8a5a1
Alexander <alxndr+github@gmail.com>
2014-11-23 00:55:14 -0800

    expect syntax

--- spec/features/static_request_spec.rb
+++ spec/features/static_request_spec.rb
@@ -8,26 +8,26 @@ feature 'home page' do
 
   describe 'content' do
     it 'asks you to type in your favorite band' do
-      page.should have_content 'Make filler text from the lyrics of:'
-      page.should have_selector 'input#artist[type=text]'
+      expect(page).to have_content 'Make filler text from the lyrics of:'
+      expect(page).to have_selector 'input#artist[type=text]'
     end
   end
 
   describe 'submitting band name' do
-    it 'sends you to a new page' do
-      VCR.use_cassette 'artist_api_responses', record: :once do
+    VCR.use_cassette 'artist_api_responses', record: :once do
+      it 'sends you to a new page' do
         fill_in 'artist', with: 'blind faith'
         fill_in 'How much text are you looking for?', with: '2'
         choose 'sentences'
         click_button 'do it'
 
-        page.should have_content 'Lorem ipsum from Blind Faith lyrics'
+        expect(page).to have_content 'Lorem ipsum from Blind Faith lyrics'
       end
     end
   end
 
   describe 'analytics' do
-    it 'tracks your every movement' do
+    it 'is on the page' do
       page.source.should match 'UA-xxxxx-y'
     end
   end
