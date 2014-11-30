53eb6b4a149ded28044b8d736f73719c7b66e094
Alexander <alxndr+github@gmail.com>
2014-11-29 21:17:39 -0800

    tidy up factories spec a bit

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -3,26 +3,32 @@ require 'spec_helper'
 non_idempotent_factories = %i(artist) # TODO artist isn't really idempotent
 broken_factories = %i()
 
-FactoryGirl.factories.map(&:name).reject { |factory_sym|
-  broken_factories.include? factory_sym
-}.each do |factory_sym|
-  factory_name = factory_sym.to_s.camelize
+def factories_except_for(broken_factories)
+  FactoryGirl.factories.map(&:name).reject { |factory_sym| broken_factories.include? factory_sym }
+end
 
-  describe "The #{factory_name} factory" do
-    let(:first) { FactoryGirl.build factory_sym }
+describe 'working factories' do
+  factories_except_for(broken_factories).each do |factory_sym|
+    factory_name = factory_sym.to_s.camelize
 
-    it 'should be valid' do
-      expect(first).to be_valid, first.errors.full_messages.to_sentence
-    end
+    describe "The #{factory_name} factory" do
+      let(:first) { FactoryGirl.build factory_sym }
 
-    describe "creating a second #{factory_name}" do
-      before do
-        first.save # save! ?
-      end
       it 'should be valid' do
-        second = FactoryGirl.build(factory_sym)
-        expect(second).to be_valid, second.errors.full_messages.to_sentence
+        expect(first).to be_valid, first.errors.full_messages.to_sentence
       end
-    end unless non_idempotent_factories.include? factory_sym
+
+      unless non_idempotent_factories.include? factory_sym
+        describe "creating a second #{factory_name}" do
+          before do
+            first.save # save! ?
+          end
+          it 'should be valid' do
+            second = FactoryGirl.build(factory_sym)
+            expect(second).to be_valid, second.errors.full_messages.to_sentence
+          end
+        end
+      end
+    end
   end
 end
