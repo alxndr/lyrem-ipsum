7988d3481e8ad3bb4fa66ea83491234c8cc4f979
Alexander <alxndr+github@gmail.com>
2014-11-29 20:33:18 -0800

    remove comments

--- spec/factories/artist_factory.rb
+++ spec/factories/artist_factory.rb
@@ -1,15 +1,7 @@
 FactoryGirl.define do
 
   factory :artist do
-    #fake_data = { albums: %w(debut sophomore swansong sharkjump) }
-
-    #name 'Frank'
     slug 'fz' # TODO should be unique
-    #data fake_data.to_json
-
-    #before(:build) do |a|
-      #a.stub(:fetch_data_for_artist).and_return fake_data
-    #end
   end
 
 end
