7c955db3e5e661ea3d93dd5386836ada1e1761e9
Alexander <alxndr+github@gmail.com>
2016-05-15 15:36:33 -0700

    whitespace

--- mix.exs
+++ mix.exs
@@ -13,25 +13,28 @@ defmodule LyremIpsum.Mixfile do
   end
 
   # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [mod: {LyremIpsum, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [
+       :cowboy,
+       :gettext,
+       :logger,
+       :phoenix,
+       :phoenix_html,
+     ]]
   end
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.4"},
-     {:phoenix_html, "~> 2.4"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:cowboy, "~> 1.0"},
+      {:gettext, "~> 0.9"},
+      {:phoenix, "~> 1.1.4"},
+      {:phoenix_html, "~> 2.4"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+    ]
   end
 end
